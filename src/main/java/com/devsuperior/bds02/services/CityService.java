5abe34868267d4c2716229af589db9b98291665b
Geovane Calheira da Silva <geovanecalheiro@gmail.com>
2022-02-22 00:52:21 -0300

    CityControllerIT - delete

--- src/main/java/com/devsuperior/bds02/services/CityService.java
+++ src/main/java/com/devsuperior/bds02/services/CityService.java
@@ -6,35 +6,50 @@ import java.util.stream.Collectors;
 import javax.transaction.Transactional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 
 import com.devsuperior.bds02.dto.CityDTO;
 import com.devsuperior.bds02.entities.City;
 import com.devsuperior.bds02.repositories.CityRepository;
+import com.devsuperior.bds02.services.exceptions.DatabaseException;
+import com.devsuperior.bds02.services.exceptions.ResourceNotFoundException;
 
 @Service
 public class CityService {
-	
+
 	@Autowired
 	private CityRepository repository;
-	
+
 	@Transactional
-	public List<CityDTO> findAll () {
-		
-		 List<City> list = repository.findAll(Sort.by("name"));
-		 return list.stream().map(x -> new CityDTO(x)).collect(Collectors.toList());
+	public List<CityDTO> findAll() {
+
+		List<City> list = repository.findAll(Sort.by("name"));
+		return list.stream().map(x -> new CityDTO(x)).collect(Collectors.toList());
 	}
 
 	@Transactional
 	public CityDTO insert(CityDTO dto) {
-		
+
 		City entity = new City();
 		entity.setName(dto.getName());
-	    entity = repository.save(entity);
-	    
+		entity = repository.save(entity);
+
 		return new CityDTO(entity);
 	}
 
+	public void delete(Long id) {
+		try {
+			repository.deleteById(id);
+		} 
+		catch (EmptyResultDataAccessException e) {
+			throw new ResourceNotFoundException("Id não encontrado");
+		}
+		catch (DataIntegrityViolationException e) {
+			throw new DatabaseException("Você não pode excluir uma entidade que possui dependência");
+		}
+	}
 	
 }
